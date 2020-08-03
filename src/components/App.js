df64c29138844b66d81df1d70c11e0da7b55dd3a
Adilet312 <adiletm@hotmail.com>
2020-08-03 02:33:53 -0700

    Add complete version of Dasboard.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,37 @@
-import React, {useState,useEffect} from 'react';
-import {BrowserRouter as Router, Switch,  Route, Link } from "react-router-dom";
-import { withRouter } from "react-router";
+import React from 'react';
 import WelcomeBack from "./WelcomeBack";
 import RecentActivity from "./RecentActivity";
 import ProgressTracker from "./ProgressTracker";
+import BlueBox from "./BlueBox";
+import OrangeBox from "./OrangeBox";
+import GreenBox from "./GreenBox";
+import RedBox from "./RedBox";
+import Table from "./Table";
 import './css/app.css';
  const App = () =>{
    return(
 <React.Fragment>
-   <header></header>
+   <header>
+     <div className="container-h">
+       <div className="head-h">
+          <h1 className="head-text-h"><i className="fas fa-wave-square"></i>Dashboard</h1>
+          <p className="text-h">Example dashboard overview and content summary</p>
+       </div>
+       <div className="calendar-h">
+          <div className="date-h"><i className="far fa-calendar-check"></i>June 22, 2020 - Jul 21, 2020<i className="fas fa-chevron-down"></i></div>
+      </div>
+    </div>
+
+   </header>
     <main>
-      <div class="welcome-back"><WelcomeBack/></div>
-      <div class="recent-activity"><RecentActivity/></div>
-      <div class="progress-tracker"><ProgressTracker/></div>
-      <div class="earnings-monthly"></div>
-      <div class="earnings-year"></div>
-      <div class="task-completion"></div>
-      <div class="pending-request"></div>
-      <div class="table"></div>
+      <div className="welcome-back"><WelcomeBack/></div>
+      <div className="recent-activity"><RecentActivity/></div>
+      <div className="progress-tracker"><ProgressTracker/></div>
+      <div className="earnings-monthly"><BlueBox/></div>
+      <div className="earnings-year"><OrangeBox/></div>
+      <div className="task-completion"><GreenBox/></div>
+      <div className="pending-request"><RedBox/></div>
+      <div className="personal-management"><Table/></div>
     </main>
    <footer></footer>
 </React.Fragment>
--- src/components/ProgressTracker.js
+++ src/components/ProgressTracker.js
@@ -1,63 +1,63 @@
-import React, {useState,useEffect} from 'react';
+import React  from 'react';
 import './css/ProgressTracker.css';
 
 const ProgressTracker = () =>{
   return(
-    <section>
-      <header>
+    <section className="section-progress-tracker">
+      <header className="section-progress-header">
       <h7>Progress Tracker</h7>
-      <div className="menu"><i class="fas fa-ellipsis-v"></i></div>
+      <div classNameName="menu"><i className="fas fa-ellipsis-v"></i></div>
       </header>
         <ul>
-          <li class="box1">
-            <div class="header">
+          <li className="box1">
+            <div className="header">
               <h7>Server MIgration</h7>
               <p>%20</p>
             </div>
-            <div class="progress-box">
-              <div class="progress-box1-color"></div>
+            <div className="progress-box">
+              <div className="progress-box1-color"></div>
             </div>
           </li>
-          <li class="box2">
-            <div class="header">
+          <li className="box2">
+            <div className="header">
               <h7>Sales Tracking</h7>
               <p>%40</p>
             </div>
-            <div class="progress-box">
-              <div class="progress-box2-color"></div>
+            <div className="progress-box">
+              <div className="progress-box2-color"></div>
             </div>
           </li>
-          <li class="box3">
-                <div class="header">
+          <li className="box3">
+                <div className="header">
               <h7>Customer Database</h7>
               <p>%60</p>
             </div>
-            <div class="progress-box">
-              <div class="progress-box3-color"></div>
+            <div className="progress-box">
+              <div className="progress-box3-color"></div>
             </div>
           </li>
-          <li class="box4">
-                <div class="header">
+          <li className="box4">
+                <div className="header">
               <h7>Payout Details</h7>
               <p>%80</p>
             </div>
-            <div class="progress-box">
-              <div class="progress-box4-color"></div>
+            <div className="progress-box">
+              <div className="progress-box4-color"></div>
             </div>
           </li>
-          <li class="box5">
-                <div class="header">
+          <li className="box5">
+                <div className="header">
               <h7>Account Setup</h7>
               <p>Completed</p>
             </div>
-            <div class="progress-box">
-              <div class="progress-box5-color"></div>
+            <div className="progress-box">
+              <div className="progress-box5-color"></div>
             </div>
           </li>
         </ul>
         <footer>
           <h7><a href="#">Visit Task Center</a></h7>
-          <div className="menu"><i class="fas fa-arrow-right"></i></div>
+          <div className="menu"><i className="fas fa-arrow-right"></i></div>
         </footer>
     </section>
   )
